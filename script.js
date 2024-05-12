ff684ab297987ddead33142de56fbbb677e37acb
PhuocLoc <lephuocloc1729@gmail.com>
2024-05-12 08:39:36 +0700

    third

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+function redirect() {
+  window.location.href = "index1.html"; // Replace 'second.html' with the path to your second HTML file
+}
+// Set timeout for 5 seconds
+setTimeout(redirect, 10000); // 5000 milliseconds = 5 seconds
